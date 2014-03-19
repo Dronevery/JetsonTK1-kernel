2f2df613dfebb821c25d381de224c58104afc039
Prabhu Kuttiyam <pkuttiyam@nvidia.com>
2014-03-18 18:11:37 -0700

    video: tegra: host: gk20a: remove pmu dump stats
    This commit removes the dumping of pmu stats only
    if the pmu exterr intr occurs.
    
    bug 1458353
    
    Change-Id: I0836da647abca64879991af9c280572477d1ce97
    Signed-off-by: Prabhu Kuttiyam <pkuttiyam@nvidia.com>
    Reviewed-on: http://git-master/r/383442
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Eric Brower <ebrower@nvidia.com>
    Reviewed-by: Winnie Hsu <whsu@nvidia.com>

--- drivers/video/tegra/host/gk20a/pmu_gk20a.c
+++ drivers/video/tegra/host/gk20a/pmu_gk20a.c
@@ -2301,7 +2301,6 @@ void gk20a_pmu_isr(struct gk20a *g)
 	if (intr & pwr_falcon_irqstat_exterr_true_f()) {
 		nvhost_err(dev_from_gk20a(g),
 			"pmu exterr intr not implemented. Clearing interrupt.");
-		pmu_dump_falcon_stats(pmu);
 
 		gk20a_writel(g, pwr_falcon_exterrstat_r(),
 			gk20a_readl(g, pwr_falcon_exterrstat_r()) &
