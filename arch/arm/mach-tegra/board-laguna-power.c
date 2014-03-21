82beee5808b7089cf21964e2b65efec4e5d6bb4e
Bibek Basu <bbasu@nvidia.com>
2014-03-21 06:30:32 +0530

    ARM: tegra: laguna: configure ams gpio1 and gpio2
    Configure ams gpio1 and 2 as bial pull-down from pdata
    
    Bug 1485412
    
    Change-Id: I8df5c6e79933e1c9c01f35ae7938bfd360fce88f
    Signed-off-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-on: http://git-master/r/384705
    Tested-by: Mike Thompson <mikthompson@nvidia.com>
    Reviewed-by: Eric Brower <ebrower@nvidia.com>
    Reviewed-by: Matthew Pedro <mapedro@nvidia.com>
    Tested-by: Matthew Pedro <mapedro@nvidia.com>

--- arch/arm/mach-tegra/board-laguna-power.c
+++ arch/arm/mach-tegra/board-laguna-power.c
@@ -205,8 +205,8 @@ AMS_PDATA_INIT(ldo11, NULL, 1800000, 1800000, 0, 0, 1, 0);
 
 static struct as3722_pinctrl_platform_data as3722_pctrl_pdata[] = {
 	AS3722_PIN_CONTROL("gpio0", "gpio", NULL, NULL, NULL, "output-low"),
-	AS3722_PIN_CONTROL("gpio1", "gpio", NULL, NULL, NULL, "output-high"),
-	AS3722_PIN_CONTROL("gpio2", "gpio", NULL, NULL, NULL, "output-high"),
+	AS3722_PIN_CONTROL("gpio1", "gpio", "pull-down", NULL, NULL, "output-high"),
+	AS3722_PIN_CONTROL("gpio2", "gpio", "pull-down", NULL, NULL, "output-high"),
 	AS3722_PIN_CONTROL("gpio3", "gpio", NULL, NULL, "enabled", NULL),
 	AS3722_PIN_CONTROL("gpio4", "gpio", NULL, NULL, NULL, "output-high"),
 	AS3722_PIN_CONTROL("gpio5", "gpio", "pull-down", NULL, "enabled", NULL),
