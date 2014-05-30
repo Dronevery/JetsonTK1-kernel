796667a918236d6c5f5e2f09e5e35c246f3487d6
Venkat Reddy Talla <vreddytalla@nvidia.com>
2014-05-30 12:18:47 +0530

    power: bq2419x:adjust input voltage limit based on battery SOC
    configure input voltage limit based on battery SOC,
    get the battery soc from fuel gauge and adjust input voltage
    as the battery soc rises when usb/NV charger cable connected.
    
    Bug 1516392
    
    Change-Id: I14e44852760f046884fd066ffcdfff86b36871c1
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: http://git-master/r/422962
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- include/linux/power/bq2419x-charger.h
+++ include/linux/power/bq2419x-charger.h
@@ -152,6 +152,9 @@ struct bq2419x_charger_platform_data {
 	u32 *chg_current_limit;
 	u32 *chg_thermal_voltage_limit;
 	u32 auto_recharge_time_supend;
+	u32 *soc_range;
+	u32 *input_voltage_soc_limit;
+	int n_soc_profile;
 };
 
 /*
