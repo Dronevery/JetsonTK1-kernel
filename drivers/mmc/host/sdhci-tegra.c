267626924064e0b7af9a07b3f15582fa5f8baf6e
R Raj Kumar <rrajk@nvidia.com>
2014-11-18 14:47:38 +0530

    mmc: tegra: Enable SD2.0 cards support
    - Enabled SD2.0 (which runs at 3.3V) on T210 based
    A01Q chips by using fuse opt_subrevision data
    - Below are the opt_subrevision values for T210 chips
      A01: 0x0
      A01P: 0x1
      A01Q: 0x2
    
    Bug 200056821
    
    Change-Id: I8d5a32090dc6823fa7fdc0896a491d9266c77218
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/604772
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -51,6 +51,7 @@
 #include <linux/tegra-pmc.h>
 
 #include <linux/platform_data/mmc-sdhci-tegra.h>
+#include <linux/platform/tegra/common.h>
 
 #include "sdhci-pltfm.h"
 
@@ -4661,6 +4662,7 @@ static struct tegra_sdhci_platform_data *sdhci_tegra_dt_parse_pdata(
 	plat->en_io_trim_volt = of_property_read_bool(np,
 			"nvidia,en-io-trim-volt");
 	plat->is_emmc = of_property_read_bool(np, "nvidia,is-emmc");
+	plat->is_sd_device = of_property_read_bool(np, "nvidia,sd-device");
 
 	if (!of_property_read_u32(np, "mmc-ocr-mask", &val)) {
 		if (val == 0)
@@ -4857,6 +4859,7 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 	const char *parent_clk_list[TEGRA_SDHCI_MAX_PLL_SOURCE];
 	int rc;
 	u8 i;
+	u32 opt_subrevision;
 
 	for (i = 0; i < ARRAY_SIZE(parent_clk_list); i++)
 		parent_clk_list[i] = NULL;
@@ -5039,6 +5042,14 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 		 */
 		tegra_host->vddio_min_uv = SDHOST_HIGH_VOLT_MIN;
 		tegra_host->vddio_max_uv = SDHOST_HIGH_VOLT_MAX;
+	}
+
+	if (plat->is_sd_device &&
+		(tegra_get_chipid() == TEGRA_CHIPID_TEGRA21) &&
+		(tegra_chip_get_revision() == TEGRA_REVISION_A01)) {
+		opt_subrevision = tegra_get_fuse_opt_subrevision();
+		if ((opt_subrevision == 0) || (opt_subrevision == 1))
+			plat->limit_vddio_max_volt = true;
 	}
 
 	if (plat->limit_vddio_max_volt) {
