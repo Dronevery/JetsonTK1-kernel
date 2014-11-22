45ffeac38723560ece94853da64a356bb12f741e
Alex Frid <afrid@nvidia.com>
2014-11-21 17:40:49 -0800

    clock: tegra21: Enable GPU debugfs rate polling
    Bug 1568176
    
    Change-Id: Ic579ba44cfb3a3275e7e7131f2027f52edd66b29
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    (cherry picked from commit 7ac9e79ac556afdbc770955ef6b0221412f7ad2b)
    Reviewed-on: http://git-master/r/655517
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -8693,6 +8693,7 @@ static struct clk_ops tegra_clk_gbus_ops = {
 };
 
 static struct raw_notifier_head gbus_rate_change_nh;
+static wait_queue_head_t gbus_poll_rate;
 
 static struct clk tegra_clk_gbus = {
 	.name      = "gbus",
@@ -8701,6 +8702,7 @@ static struct clk tegra_clk_gbus = {
 	.max_rate  = 1300000000,
 	.shared_bus_flags = SHARED_BUS_RETENTION,
 	.rate_change_nh = &gbus_rate_change_nh,
+	.debug_poll_qh  = &gbus_poll_rate,
 };
 
 static void tegra21_camera_mclk_init(struct clk *c)
