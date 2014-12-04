3be03209498e8a68176da2065354b860ea658cba
Wenjia Zhou <wenjiaz@nvidia.com>
2014-12-03 18:07:19 -0800

    video: tegra: host: t210: Change vic.emc default
    t21_vic_info has emc clock default as UINT_MAX, which pushes vic.emc to 1.6 Ghz
    during camera vdieo recording. This change brings down the vic.emc and reduces
    the power consumption.
    
    Bug 1585146
    
    Change-Id: I25c926c98ee1c368f0e9c5706ee7fb7817faec70
    Signed-off-by: Wenjia Zhou <wenjiaz@nvidia.com>
    Reviewed-on: http://git-master/r/659244
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- drivers/video/tegra/host/t210/t210.c
+++ drivers/video/tegra/host/t210/t210.c
@@ -397,7 +397,7 @@ struct nvhost_device_data t21_vic_info = {
 	.num_channels		= 1,
 	.modulemutexes		= {NVMODMUTEX_VIC},
 	.clocks			= {{"vic03", UINT_MAX, 0, TEGRA_MC_CLIENT_VIC},
-				   {"emc", UINT_MAX,
+				   {"emc", HOST_EMC_FLOOR,
 				   NVHOST_MODULE_ID_EXTERNAL_MEMORY_CONTROLLER},
 				   {"vic_floor", 0,
 				   NVHOST_MODULE_ID_CBUS_FLOOR},
