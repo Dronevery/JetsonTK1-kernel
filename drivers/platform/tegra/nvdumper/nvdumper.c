416edbed72ef47bf3cb1b0204894529c51b7e5bb
Yifei Wan <ywan@nvidia.com>
2014-12-05 23:30:15 -0600

    nvdumper: moved control from debugfs to sysfs
    Change-Id: Ieef0e1a56355b06eaeaea90bd08297b9db5374ad
    Signed-off-by: Yifei Wan <ywan@nvidia.com>
    Reviewed-on: http://git-master/r/660512
    (cherry picked from commit 90496b08a89e42e42c11911cb42cb0faef64ea47)
    Reviewed-on: http://git-master/r/670274
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mark Peters <mpeters@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/platform/tegra/nvdumper/nvdumper.c
+++ drivers/platform/tegra/nvdumper/nvdumper.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/nvdumper.c
  *
- * Copyright (c) 2011-2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
