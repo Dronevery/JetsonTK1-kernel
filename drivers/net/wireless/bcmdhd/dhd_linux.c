f013ea77160e9291efd3f0a506f2d62c3d387e92
Shital Jaju <shital.jaju@broadcom.com>
2015-02-03 17:25:28 +0530

    net: wireless: bcmdhd: Enable rxcb iovar
    Enable the rxcb in firmware to avoid the "out of bus credits"
    condition which was causing the dhd hang condition.
    
    Bug 1594708
    
    Change-Id: I7f89f8b1fbc092a65cc1ea5155592fb3e15b7903
    Signed-off-by: Raghavender <kraghavender@nvidia.com>
    Reviewed-on: http://git-master/r/680606
    Reviewed-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-by: Narayan Reddy <narayanr@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -5194,6 +5194,9 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 #ifdef WLTDLS
 	dhd->tdls_enable = FALSE;
 #endif /* WLTDLS */
+#ifdef RXCB
+	uint32 rxcb = 1;
+#endif
 	dhd->suspend_bcn_li_dtim = CUSTOM_SUSPEND_BCN_LI_DTIM;
 	DHD_TRACE(("Enter %s\n", __FUNCTION__));
 	dhd->op_mode = 0;
@@ -5785,6 +5788,12 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 #ifdef WL11U
 	dhd_interworking_enable(dhd);
 #endif /* WL11U */
+#ifdef RXCB
+	/* Enable bus rx callback */
+	bcm_mkiovar("bus:rxcb", (char *)&rxcb, 4, iovbuf, sizeof(iovbuf));
+	if ((ret =dhd_wl_ioctl_cmd(dhd, WLC_SET_VAR, iovbuf, sizeof(iovbuf), TRUE, 0)) < 0)
+		DHD_ERROR(("%s failed to set RXCB %d\n", __FUNCTION__, ret));
+#endif
 
 done:
 	return ret;
