cf0c21a126067c84bc71094428d999e18a938584
Eric Miao <emiao@nvidia.com>
2015-02-25 12:19:12 -0800

    arm: tegra: no need to reserve fbmem
    Bug 1602113
    
    Now framebuffer memory is allocated by the driver, there is no
    need to reserve by tegra_reserve4().
    
    Change-Id: I4e2e025f17c32d338d15269aed65058498a7cb30
    Signed-off-by: Eric Miao <emiao@nvidia.com>
    Reviewed-on: http://git-master/r/711712
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Li Li (SW-TEGRA) <lli5@nvidia.com>
    Tested-by: Li Li (SW-TEGRA) <lli5@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm/mach-tegra/board-panel.h
+++ arch/arm/mach-tegra/board-panel.h
@@ -115,8 +115,6 @@ int tegra_panel_reset(struct tegra_panel_of *panel, unsigned int delay_ms);
 int tegra_init_hdmi(struct platform_device *pdev,
 			struct platform_device *phost1x);
 
-void tegra_fb_copy_or_clear(void);
-
 int tegra_disp_defer_vcore_override(void);
 
 void tegra_set_fixed_panel_ops(bool is_primary,
