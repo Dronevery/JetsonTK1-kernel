b69897d71c8f6119b49ceb8d3273cdb354178cc5
Seshendra Gadagottu <sgadagottu@nvidia.com>
2015-02-25 17:38:05 -0800

    gpu: nvgpu: support for dumping vpr/wpr info
    Added support for dumping vpr/wpr info for gm20b.
    This dump info called when ever gk20a_mm_fb_flush
    is timed-out.
    
    Bug 200082817
    
    Change-Id: I21b0372d0e3f976a189c9c428c015165b715bf88
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/711439
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/gk20a.h
+++ drivers/gpu/nvgpu/gk20a/gk20a.h
@@ -1,7 +1,7 @@
 /*
  * GK20A Graphics
  *
- * Copyright (c) 2011-2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -158,6 +158,7 @@ struct gpu_ops {
 		void (*init_uncompressed_kind_map)(struct gk20a *g);
 		void (*init_kind_attr)(struct gk20a *g);
 		void (*set_mmu_page_size)(struct gk20a *g);
+		void (*dump_vpr_wpr_info)(struct gk20a *g);
 	} fb;
 	struct {
 		void (*slcg_bus_load_gating_prod)(struct gk20a *g, bool prod);
