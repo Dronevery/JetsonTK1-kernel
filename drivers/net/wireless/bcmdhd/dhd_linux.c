724d32892c2574f75d90bc9bd1bc42b81a0e87d5
Rakesh Goyal <rgoyal@nvidia.com>
2015-05-05 18:51:07 +0530

    net: wireless: bcmdhd: capture driver suspend/resume call
    1) MAC events to driver
    2) WL_control packet request
    3) resume by wifi (bcmdhd)
    4) whenever there dpd_thread get scheduled
    (currently disable but required framework present)
    
    Bug 200042859
    
    Change-Id: I5624fe017ff2508a11c06455549d968c5375b8bf
    Signed-off-by: Rakesh Goyal <rgoyal@nvidia.com>
    Reviewed-on: http://git-master/r/742614
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -116,9 +116,16 @@
 		tegra_sysfs_histogram_tcpdump_rx(skb, __func__, __LINE__);\
 	}\
 
+#define DPC_CAPTURE(void)\
+	{\
+		tegra_sysfs_dpc_pkt();\
+	} \
+
 #else
 
 #define RX_CAPTURE(skb)
+
+#define DPC_CAPTURE(void)
 
 #endif
 
@@ -3046,6 +3053,7 @@ dhd_dpc_thread(void *data)
 				timeout = jiffies + msecs_to_jiffies(100);
 				loopcnt = 0;
 				count = 0;
+				/* DPC_CAPTURE(); */
 				while (dhd_bus_dpc(dhd->pub.bus)) {
 					++loopcnt;
 					if (time_after(jiffies, timeout) &&
