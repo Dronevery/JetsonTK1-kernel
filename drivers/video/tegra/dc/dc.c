45434628f4672cabf44e7fd36b7bfcc8167459b2
Ivan Raul Guadarrama <iguadarrama@nvidia.com>
2015-06-23 13:52:56 +0300

    tegra: disp: dc: add persistence to virtual edid
    Change virtual edid to persist across software hotplugs.
    Before, the virtual edid was lost after an unplug event.
    Only initialize the virtual edid state on probe.
    
    Bug 1613815
    
    Change-Id: Ied7ed73d4267f880e62e0ac35fe11c1297032a5f
    Signed-off-by: Ivan Raul Guadarrama <iguadarrama@nvidia.com>
    Reviewed-on: http://git-master/r/761240
    (cherry picked from commit 073a1adb11c6d327e32270c6600c47b00296a53e)
    Reviewed-on: http://git-master/r/764754
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -3189,9 +3189,6 @@ static int tegra_dc_init(struct tegra_dc *dc)
 
 	tegra_dc_io_end(dc);
 
-	dc->vedid = false;
-	dc->vedid_data = NULL;
-
 	return 0;
 }
 
@@ -4409,6 +4406,13 @@ static int tegra_dc_probe(struct platform_device *ndev)
 	 * boot. It should not apply for e.g. HDMI hotplug.
 	 */
 	dc->initialized = false;
+
+	/*
+	 * Initialize vedid state. This is placed here
+	 * to allow persistence across sw HDMI hotplugs.
+	 */
+	dc->vedid = false;
+	dc->vedid_data = NULL;
 
 	return 0;
 
