78d9ee9ad113e31193fdfae30f3743a0febcb8ca
Ankita Garg <ankitag@nvidia.com>
2015-08-25 16:15:03 -0700

    HID: hidraw: Fix potential memory leak in list->buffer
    Bug 1677312
    
    Change-Id: I4fe6296b3d944c7ebf81f3af2aa73256688a4613
    Signed-off-by: Ankita Garg <ankitag@nvidia.com>
    Reviewed-on: http://git-master/r/789231
    Reviewed-by: Ajay Gupta <ajayg@nvidia.com>
    Reviewed-by: Petlozu Pravareshwar <petlozup@nvidia.com>
    Reviewed-by: Raymond Poudrier <rapoudrier@nvidia.com>

--- drivers/hid/hidraw.c
+++ drivers/hid/hidraw.c
@@ -491,6 +491,8 @@ int hidraw_report_event(struct hid_device *hid, u8 *data, int len)
 		if (new_head == list->tail)
 			continue;
 
+		if (list->buffer[list->head].value)
+			kfree(list->buffer[list->head].value);
 		if (!(list->buffer[list->head].value = kmemdup(data, len, GFP_ATOMIC))) {
 			ret = -ENOMEM;
 			break;
